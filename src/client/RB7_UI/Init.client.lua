5b8c9f07d0b33b3c061834ebf9eda4f898f8c321
same <Samuel.sohn.hd@gmail.com>
2025-08-21 12:33:21 +0200

    autosave: 2025-08-21 12:33:21

--- src/client/RB7_UI/Init.client.lua
+++ src/client/RB7_UI/Init.client.lua
@@ -3,9 +3,8 @@ local LobbyScreen = require(script.Parent.screens.LobbyScreen)
 local Loadout = require(script.Parent.screens.LoadoutScreen)
 
 local lobby = LobbyScreen()
-local loadoutGui, _ = Loadout()
+local loadoutGui = (select(1, Loadout()))
 
--- Navigation: Klick auf "LOADOUT" öffnet den Loadout-Screen
 for _,desc in ipairs(lobby:GetDescendants()) do
     if desc:IsA("TextButton") and desc.Text == "LOADOUT" then
         desc.MouseButton1Click:Connect(function()
